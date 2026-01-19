3da965acf50fb93c67af803e809d3622849cf603
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-01-19 16:33:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "myinstance" {
+  ami           = "ami-0ced6a024bb18ff2e"
+  instance_type = "t3.micro"
+
+  tags = {
+    name = "mydemo"
+  }
+}
