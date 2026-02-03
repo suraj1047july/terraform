7ede1d771c00962cccbada9cf876117e4eeefb16
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-02-03 12:41:16 +0530

    Update main.tf

--- azure/firewall-module/main.tf
+++ azure/firewall-module/main.tf
@@ -1,1 +1,13 @@
+resource "azurerm_firewall" "example" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = var.sku_name
+  sku_tier            = var.tier
 
+  ip_configuration {
+    name                 = var.name
+    subnet_id            = var.subnet
+    public_ip_address_id = var.public_ip
+  }
+}
