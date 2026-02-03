36c24ae1f7af01ad54ed9405732c32602b716507
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-02-03 12:45:57 +0530

    Update main.tf

--- azure/firewall-module/main.tf
+++ azure/firewall-module/main.tf
@@ -1,13 +1,23 @@
-resource "azurerm_firewall" "example" {
-  name                = var.name
+resource "azurerm_firewall_policy" "firewall_policy" {
+  name                = var.firewall_policy_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.firewall_policy_sku
+}
+
+resource "azurerm_firewall" "AZ_firewall" {
+  name                = var.firewall_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku_name            = var.sku_name
-  sku_tier            = var.tier
+  sku_name            = var.firewall_sku_name
+  sku_tier            = var.firewall_sku_tier
+  firewall_policy_id  = azurerm_firewall_policy.this.id
 
   ip_configuration {
-    name                 = var.name
-    subnet_id            = var.subnet
-    public_ip_address_id = var.public_ip
+    name                 = "firewall-ipconfig"
+    subnet_id            = var.firewall_subnet_id
+    public_ip_address_id = var.public_ip_id
   }
+
+  tags = var.tags
 }
