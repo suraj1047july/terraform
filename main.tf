49508d11d16856322fa3c50f1532bb9a6782cf26
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-02-24 11:38:43 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "myinstance" {
+  ami           = "ami-0ced6a024bb18ff2e"
+  instance_type = "t3.micro"
+
+  tags = {
+    name = "mydemo"
+  }
+  
+metadata_options {
+    http_tokens = "required"
+  }
+}
