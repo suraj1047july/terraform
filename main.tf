d235b227f83a47e5ed7f5eea7921500e8fff9036
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-10 10:31:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
-resource "aws_instance" "myinstance" {
-  ami           = "ami-0ced6a024bb18ff2e"
-  instance_type = "t3.micro"
-
-  tags = {
-    name = "mydemo"
-  }
-  
-metadata_options {
-    http_tokens = "required"
-  }
+module "vpc" {
+  source  = "app.terraform.io/terraform_learn_all_cloud/vpc/aws"
+  version = "1.0.0"
+  for_each =var.vpcs
+  cidr = each.value.cidr
+  name = "${each.value.name}"
 }
-
-
-
-
+module "subnet" {
+  source  = "app.terraform.io/terraform_learn_all_cloud/subnet/aws"
+  version = "1.0.0"
+  # insert required variables here
+  for_each =var.subnets
+  cidr_blocks = each.value.cidr_blocks
+  vpc_id = module.vpc[each.value.vpc_key].vpc_id
+  name = each.value.name
+  azs = ["ap-south-1a", "ap-south-1b"]
+  public = "true"
+}
