8bd18f48e107082d85e6fab0b4bb3f1485f7d047
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-23 14:59:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,10 @@ module "vnet" {
   source  = "app.terraform.io/terraform_learn_all_cloud/vnet/azurerm"
   version = "1.0.2"
   address_space = var.address_space
-  location = "Central India"
+  location = var.location
   name = var.name
   resource_group_name = var.rg
-  tags = var.tags
+    tags = {
+    env = "test"
+  }
 }
