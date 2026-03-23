4f566d7a36a3d944496a959cfd4c5f605ab4c9bf
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-23 15:38:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "address_space" {
-  description = "Address space for VNet"
-  type        = string
-}
 
 variable "name" {
   description = "VNet name"
@@ -17,7 +13,10 @@ variable "location" {
   description = "azure region"
   type        = string
 }
+variable "address_space" {
+  type = list(string)
+}
+
 variable "tags" {
-  description = "tags"
-  type        = string
+  type = map(string)
 }
