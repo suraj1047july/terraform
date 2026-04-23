bd0a8d2e261a9f5373d39e1b811f24ef51576103
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-04-23 15:44:30 +0530

    Add Resource Group module to main.tf
    Fix typo in resource group module configuration.

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+module "Resource-Group" {
+  source  = "app.terraform.io/terraform_learn_all_cloud/Resource-Group/azurerm"
+  version = "1.0.3"
+  name = var.rgname
+  location - var.location
+}
+
 module "vnet" {
   source  = "app.terraform.io/terraform_learn_all_cloud/vnet/azurerm"
   version = "1.0.2"
