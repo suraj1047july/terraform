7ff9f9c2e83a242e522c6a1d72951c4b372a8592
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-05-08 11:20:42 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "vnet_id" {
-  value = module.vnet.vnet_id
-}
+    value = module.vnet.id
+    }
+output "vnet_name" {
+    value = module.vnet.name
+    }           
+output "rg_id" {
+    value = module.ResourceGroup.id
+    }
