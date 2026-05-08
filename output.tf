5f78a123c00a4672d51e527a719073a436033cbf
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-05-08 15:58:54 +0530

    Remove unused output variables from output.tf
    Removed outputs for 'vnet_name' and 'rg_name'.

--- output.tf
+++ output.tf
@@ -1,9 +1,6 @@
 output "vnet_id" {
     value = module.vnet.id
-    }
-output "vnet_name" {
-    value = module.vnet.name
-    }           
-output "rg_name" {
-    value = module.ResourceGroup.name
+    }          
+output "rg_id" {
+    value = module.ResourceGroup.id
     }
