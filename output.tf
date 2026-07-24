a9ed3940288d74e42aeb8c3222e1479fa5e8b4f5
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 12:31:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 #output "vnet_id" {
 #    value = module.vnet.id
 #    }          
-output "resource_group" {
-  value = azurerm_resource_group.rg_name.name
+output "resource_group_id" {
+  value = module.ResourceGroup.rg_id
 }
