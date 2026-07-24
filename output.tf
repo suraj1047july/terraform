071cb144c00bc570a165b4e9a9a4d6d314fcf84e
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 16:54:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "vnet_ids" {
-  value = {
-    for k, v in module.Vnet :
-    k => v.vnet_id
-  }
-}         
-output "resource_group_id" {
-  value = module.ResourceGroup.rg_id
-}
+// output "vnet_ids" {
+//   value = {
+//     for k, v in module.Vnet :
+//     k => v.vnet_id
+//   }
+// }         
+// output "resource_group_id" {
+//   value = module.ResourceGroup.rg_id
+// }
