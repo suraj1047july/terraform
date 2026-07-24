9dc38735e516429646e09452dd124d9dc612d60b
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 16:28:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "ResourceGroup" {
   source  = "app.terraform.io/terraform_learn_all_cloud/ResourceGroup/azurerm"
-  version = "1.0.0"
+  version = "1.0.2"
   name = var.rg_name
   location = var.location
 }
 module "Vnet" {
   source  = "app.terraform.io/terraform_learn_all_cloud/Vnet/azurerm"
-  version = "1.0.0"
+  version = "1.0.2"
   for_each = var.vnets
   name           = each.value.name
   location       = each.value.location
