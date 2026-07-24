dcf46a0ee73d62dbbe9db2fcb220e604d6b165fd
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 16:33:44 +0530

    Rename resource_group to resource_group_name

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "Vnet" {
   for_each = var.vnets
   name           = each.value.name
   location       = each.value.location
-  resource_group = module.ResourceGroup.rg_id
+  resource_group_name = module.ResourceGroup.rg_name
   address_space  = each.value.address_space
 }
 
