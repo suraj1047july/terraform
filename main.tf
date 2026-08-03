adf03b0f8cb2e56c5ed64ac891dd94435abb776a
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-03 11:59:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,28 @@
-// module "ResourceGroup" {
-//   source  = "app.terraform.io/terraform_learn_all_cloud/ResourceGroup/azurerm"
-//   version = "1.0.2"
-//   name = var.rg_name
-//   location = var.location
-// }
-// module "Vnet" {
-//   source  = "app.terraform.io/terraform_learn_all_cloud/Vnet/azurerm"
-//   version = "1.0.2"
-//   for_each = var.vnets
-//   name           = each.value.name
-//   location       = module.ResourceGroup.location
-//   resource_group_name = module.ResourceGroup.rg_name
-//   address_space  = each.value.address_space
-// } 
+module "ResourceGroup" {
+  source  = "app.terraform.io/terraform_learn_all_cloud/ResourceGroup/azurerm"
+  version = "1.0.3"
+  name = var.rg_name
+  location = var.location
+}
+module "Vnet" {
+  source  = "app.terraform.io/terraform_learn_all_cloud/Vnet/azurerm"
+  version = "1.0.3"
+  for_each = var.vnets
+  name           = each.value.name
+  location       = module.ResourceGroup.location
+  resource_group_name = module.ResourceGroup.rg_name
+  address_space  = each.value.address_space
+} 
+module "RouteTable" {
+  source  = "app.terraform.io/terraform_learn_all_cloud/RouteTable/azurerm"
+  version = "1.0.3"
+  for_each = var.route_tables
+  name                = each.value.name
+  location            = module.ResourceGroup.location
+  resource_group_name = module.ResourceGroup.rg_name  
+  routes = each.value.routes
+}
+
 
 
 
