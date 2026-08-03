e92af169a303ecb158c7a2c1fd5aa9f81a75bbd4
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-03 12:09:30 +0530

    Add outputs for vnet_ids and resource_group_id
    Added output for route table IDs with description.

--- output.tf
+++ output.tf
@@ -1,9 +1,17 @@
-// output "vnet_ids" {
-//   value = {
-//     for k, v in module.Vnet :
-//     k => v.vnet_id
-//   }
-// }         
-// output "resource_group_id" {
-//   value = module.ResourceGroup.rg_id
-// }
+output "vnet_ids" {
+  value = {
+    for k, v in module.Vnet :
+    k => v.vnet_id
+  }
+}         
+output "resource_group_id" {
+  value = module.ResourceGroup.rg_id
+}
+output "route_table_ids" {
+  description = "IDs of all Route Tables"
+
+  value = {
+    for key, rt in module.RouteTable :
+    key => rt.id
+  }
+}
