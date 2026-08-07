2d1e622ed02107e234c857c03c18026304aa7c57
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-07 15:41:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -33,6 +33,6 @@ variable "subnets" {
     subnet_name      = string
     resource_group   = string
     vnet             = string
-    subnet_address_space = list(string)
+    address_prefix = list(string)
   }))
 }
