bd938a45883a206cf66b5c28f3b26c9e4b8c4068
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-17 16:00:07 +0530

    Define variables in variables.tf
    Uncommented and defined variables for resource group, location, VNets, route tables, subnets, and NSGs.

--- variables.tf
+++ variables.tf
@@ -1,55 +1,55 @@
-// variable "rg_name" {
-//   description = "Name of the resource group"
-//   type        = string
-// }
+variable "rg_name" {
+  description = "Name of the resource group"
+  type        = string
+}
 
-// variable "location" {
-//   description = "Location of the resource group"
-//   type        = string
-// }
+variable "location" {
+  description = "Location of the resource group"
+  type        = string
+}
 
-// variable "vnets" {
-//   description = "Map of VNets to create"
+variable "vnets" {
+  description = "Map of VNets to create"
 
-//   type = map(object({
-//     name           = string
-//     address_space  = list(string)
-//   }))
-// }
-// variable "route_tables" {
-//   description = "Map of Route Tables"
+  type = map(object({
+    name           = string
+    address_space  = list(string)
+  }))
+}
+variable "route_tables" {
+  description = "Map of Route Tables"
 
-//   type = map(object({
-//     name           = string
-//     route_name     = string
-//     address_prefix = string
-//     next_hop_type  = string
-//   }))
-// }
-// variable "subnets" {
-//   description = "Map of Subnets"
+  type = map(object({
+    name           = string
+    route_name     = string
+    address_prefix = string
+    next_hop_type  = string
+  }))
+}
+variable "subnets" {
+  description = "Map of Subnets"
 
-//   type = map(object({
-//     name             = string
-//     vnet             = string
-//     address_prefixes = list(string)
-//     route_table      = optional(string)
-//     nsg              = optional(string)
-//   }))
-// }
-// variable "NSGs" {
-//   description = "A map of NSG configurations"
-//   type = map(object({
-//     name                       = string
-//     rule_name                  = string
-//     priority                   = number
-//     direction                  = string
-//     access                     = string
-//     protocol                   = string
-//     source_port_range          = string
-//     destination_port_range     = string
-//     source_address_prefix      = string
-//     destination_address_prefix = string
-//   }))
-// }
+  type = map(object({
+    name             = string
+    vnet             = string
+    address_prefixes = list(string)
+    route_table      = optional(string)
+    nsg              = optional(string)
+  }))
+}
+variable "NSGs" {
+  description = "A map of NSG configurations"
+  type = map(object({
+    name                       = string
+    rule_name                  = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+}
 
